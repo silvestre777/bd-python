c8bb715796422510a7b60c1d4933f9df56dff0c6
Silvestre Ferreira <168732596+silvestre777@users.noreply.github.com>
2024-10-14 12:22:31 +0000

    Att

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ from sqlalchemy import create_engine, Column, String, Integer
 from sqlalchemy.orm import sessionmaker, declarative_base
 
 #Criando banco de dados.
-MEU_BANCO = create_engine("sqlite://meubanco.db")
+MEU_BANCO = create_engine("sqlite:///meubanco.db")
 
 #Criando conexão com banco de dados.
 Session = sessionmaker(bind=MEU_BANCO)
@@ -32,10 +32,22 @@ session = Session()
 # Criando tabela.
 
 Base = declarative_base()
-class Cliente(Base):
-    __tablename__ = "clientes"
+class Cliente(Base): # nome de classe no singular
+    __tablename__ = "clientes" # definir
     
 	#Definindo campos da tabela
+    """autoincrement = 
+    	id - nome
+        
+        1- "Silvestre"
+        2- "Fereira"
+        
+        salvar (1, "Silvestre")
+        salvar ("Ferreira")
+        
+        o autoincrement ele automatiza o processo e tabela td
+        
+        """
     id = Column("id", Integer, primary_key= True, autoincrement=True)
     nome = Column("nome", String)
     email = Column("email", String)
@@ -45,5 +57,31 @@ class Cliente(Base):
         self.nome = nome
         self.email = email
         self.senha = senha
-        
-Base.metadata.create_all(bind = MEU_BANCO)
+#Criando tabela no banco de dados.
+Base.metadata.create_all(bind=MEU_BANCO)
+
+#CRUD.
+#Creat - Insert - Salvar.
+
+#Limpando terminal
+os.system("cls || clear")
+
+print("Solicitando dados para o usuário.")
+
+inserir_nome = input("Digite seu nome: ")
+inserir_email = input("Digite seu e-mail: ")
+inserir_senha = input("Digite sua senha: ")
+
+cliente = Cliente(nome = inserir_nome, email = inserir_email, senha = inserir_senha)
+
+session.add(cliente)
+session.commit()
+
+
+#Read - Select - Consulta
+
+print("\nExibindo dados de todos os clientes.")
+lista_clientes = session.query(Cliente).all() #Consulta ao banco de dados "query" esconde o select - e vc seleciona a tabela que vc deseja que ele puxe
+
+for cliente in lista_clientes:
+    print (f"{cliente.id} - {cliente.nome} - {cliente.email} - {cliente.senha} ")
